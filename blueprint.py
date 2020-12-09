26048ff3fdbd98e7e445a6e07ab209b417af2d2d
Eshan Sharma <eshan.sharma108@gmail.com>
2020-12-09 22:14:19 +0530

    Reducing code reduncency

--- blueprint.py
+++ blueprint.py
@@ -6,6 +6,7 @@ import new_user_credentials as nuc
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import current_app as app
 from models import individual
+#from models import doctor
 from flask_mail import Mail, Message
 from PIL import Image, ImageDraw
 import qrcode
