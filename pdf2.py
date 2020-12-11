f08f90b6f5b099ec0379fceaa0112736e2b34225
Unknown <unknown@example.com>
2020-12-11 21:20:03 +0530

    NDHP PDF

--- pdf2.py
+++ pdf2.py
@@ -18,11 +18,11 @@ def send_pdf(individual x):
 	pdf.set_font("Arial", size = 15) 
 	  
 	# create a cell 
-	pdf.cell(200, 10, txt = "GeeksforGeeks",  
+	pdf.cell(200, 10, txt = "NATIONAL DIGITAL HEALTH PORTAL",  
 	         ln = 1, align = 'C') 
 	  
 	# add another cell 
-	pdf.cell(200, 10, txt = "A Computer Science portal for geeks.", 
+	pdf.cell(200, 10, txt = "DIGITAL HEALTH CARD.", 
 	         ln = 2, align = 'C') 
 	  
 	# save the pdf with name .pdf 
