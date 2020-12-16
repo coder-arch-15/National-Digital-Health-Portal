0688b4727191e8577a0987e4695fc37fa5013f90
Eshan Sharma <eshan.sharma108@gmail.com>
2020-12-16 21:00:47 +0530

    Update register_blueprints.py

--- register_blueprints.py
+++ register_blueprints.py
@@ -70,7 +70,7 @@ def sub():
 @register_bp.route('/doctor_form_submit', methods = ['GET' , 'POST'])
 def doctor_reg_sub():
 	if request.method == 'POST':
-		try:
+		# try:
 			fname = request.form['fname']
 			lname = request.form['lname']
 			email = request.form['email']
@@ -106,9 +106,9 @@ def doctor_reg_sub():
 			mail.send(msg)
 			return render_template('thank.html',namee=thank_msg)
 
-		except Exception as e:
-			msg = e
-			return render_template('thank.html',namee=msg)
+		# except Exception as e:
+		# 	msg = e
+		# 	return render_template('thank.html',namee=msg)
 ###########################################################################
 
 @register_bp.route('/labs_form_submit', methods = ['GET' , 'POST'])
