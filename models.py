a3ec103644b4742acbf0cba6bdaf2f2e28911fc0
Eshan Sharma <eshan.sharma108@gmail.com>
2020-12-16 20:24:57 +0530

    DOCTOR

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_sqlalchemy  import SQLAlchemy
-from fpdf import FPDF 
+from fpdf import FPDF
 
 # Globally accessible libraries
 db = SQLAlchemy()
@@ -28,28 +28,28 @@ class individual(UserMixin, db.Model):
 		name = self.fname + " " + self.lname
 		return name
 	def send_pdf_indi(self):
-		pdf = FPDF() 
-		  
-		# Add a page 
-		pdf.add_page() 
-		  
-		# set style and size of font  
-		# that you want in the pdf 
-		pdf.set_font("Arial", size = 15) 
-		  
-		# create a cell 
-		pdf.cell(200, 10, txt = "GeeksforGeeks",  
-		         ln = 1, align = 'C') 
-		  
-		# add another cell 
-		pdf.cell(200, 10, txt = "A Computer Science portal for geeks.", 
-		         ln = 2, align = 'C') 
+		pdf = FPDF()
+
+		# Add a page
+		pdf.add_page()
+
+		# set style and size of font
+		# that you want in the pdf
+		pdf.set_font("Arial", size = 15)
+
+		# create a cell
+		pdf.cell(200, 10, txt = "GeeksforGeeks",
+		         ln = 1, align = 'C')
+
+		# add another cell
+		pdf.cell(200, 10, txt = "A Computer Science portal for geeks.",
+		         ln = 2, align = 'C')
 		pdf.cell(200,10, txt="name"+self.get_name(), ln=3, align='C')
-		  
-		# save the pdf with name .pdf 
+
+		# save the pdf with name .pdf
 		data = self.get_name()+".pdf"
 		path = "C:\\minor_project\\"+"GFG.pdf"
-		pdf.output(path, 'D')    
+		pdf.output(path, 'D')
 
 
 
@@ -66,8 +66,9 @@ class doctor(UserMixin, db.Model):
 	gender = db.Column(db.String(30), index = False, nullable= False)
 	regnum = db.Column(db.Integer(), index = False, nullable= False)
 	Specialization = db.Column(db.Text(30), index = False, nullable= False)
+	add = db.Column(db.String(30), index = False, nullable= False)
 	state = db.Column(db.String(15), index = False, nullable= False)
-	city = db.Column(db.String(30), index = False, nullable= False)
+	city = db.Column(db.String(15), index = False, nullable= False)
 	district = db.Column(db.String(30), index = False, nullable= True)
 	pin = db.Column(db.Integer(), index = False, nullable= False)
 	addr1 = db.Column(db.String(30), index = False, nullable= False)
