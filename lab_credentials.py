65109bf275125f849fd136385e09cee3dfb7f597
Unknown <unknown@example.com>
2020-12-16 19:28:21 +0530

    lab

--- lab_credentials.py
+++ lab_credentials.py
@@ -17,12 +17,12 @@ class New_user:
 			New_user.numCounter=0
 			New_user.aplh1 = chr(ord(New_user.aplh1) + 1)
 			if(ord(New_user.aplh1)==123):
-				New_user.aplh1 = "B
+				New_user.aplh1 = "B"
 				New_user.aplh2 = chr(ord(New_user.aplh2) + 1)
 				if(ord(New_user.aplh2)==123):
 					New_user.aplh2 = "A"
 					New_user.aplh3 = chr(ord(New_user.aplh3) + 1)
-		self.unique_id = "L+New_user.Codes[city.upper()] + New_user.aplh3 + New_user.aplh2 + New_user.aplh1 + New_user.Codes[owner_name.upper()]
+		self.unique_id = "L"+New_user.Codes[city.upper()] + New_user.aplh3 + New_user.aplh2 + New_user.aplh1 + New_user.Codes[owner_name.upper()]
 		self.pasw = mob
 
 	def create_user(nu):
